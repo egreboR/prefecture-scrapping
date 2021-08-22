cdf5c09129c5f33ee0576b3a619bd0783dc487de
Benoit Roberge <benoit.roberge05@gmail.com>
2021-08-22 11:26:33 +0200

    NEw version with loop to make sure the page has changed

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ import time, datetime, random, codecs
 
 ### need configuration file
 i = 0
-jit_s = 4
-s = 10
-jit_l = 30
-l = 60
+jit_s = 2
+s = 15
+jit_l = 10
+l = 30
 links = ["www.essonne.gouv.fr/booking/create/23014/0"]
-
+NTRIAL = 5
 
 dir = "/home/data/"
-
+# dir = "./"
 
 for link in links :
     view = {'output': dir + "prefecture_" ,
@@ -39,81 +39,124 @@ with webdriver.Firefox(firefox_options=firefox_options) as driver:
     driver.set_window_size(view['width'], view['height'])
     driver.get(linkWithProtocol)
     time.sleep(l+random_number_gen(jit_l))
-
-    
-
-    with codecs.open('test.html','w+',"utf-8") as fid:
-        fid.write(driver.page_source)
+    page_1 = "Cliquez la case pour accepter les conditions d'utilisation avant de continuer le processus de prise de rendez-vous."
+    page_2 = "Choix de la nature du rendez-vous"
 
     while True:
 
+
+
         timestamp = datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+        print(timestamp)
 
-        
-        driver.set_window_size(view['width'], view['height'])
-        driver.get(linkWithProtocol)
         time.sleep(l+random_number_gen(jit_l))
-
+        driver.set_window_size(view['width'], view['height'])
         try:
-            condition = driver.find_element_by_id("condition")
-            nextButton = driver.find_element_by_name("nextButton")
+            driver.get(linkWithProtocol)
         except:
-            condition = None
-            nextButton = None
-
-        if not condition or not nextButton:
             continue
 
-        condition.click()
-        nextButton.click()
-        time.sleep(s+random_number_gen(jit_s))
+        time.sleep(l+random_number_gen(jit_l))
+        ### test if we are in the page
 
+        isloaded = False
+        trial = 0
+        while trial < NTRIAL :
 
-        try :
-            guichet_list = driver.find_elements_by_class_name("radio")
-            nextButton = driver.find_element_by_name("nextButton")
-        except:
-            guichet_list = None
-            nextButton = None
+            trial+=1
+            time.sleep(s+random_number_gen(jit_s))
+            try:
+                condition = driver.find_element_by_id("condition")
+                nextButton = driver.find_element_by_name("nextButton")
+            except:
+                if not isloaded:
+                    condition = None
+                    nextButton = None
+                    continue
+                break
 
-        if not guichet_list or not nextButton:
+            if not condition and not nextButton:
+                if not isloaded:
+                    continue
+                break
+
+            if not condition.get_attribute("title") == page_1:
+                if not isloaded:
+                    continue
+                break
+
+            try:
+                condition.click()
+                nextButton.click()
+                isloaded = True
+            except:
+                driver.save_screenshot(view["output"] + "page_01_error_01" + timestamp + '.png')
+                
+
+        if trial >= NTRIAL:
+            driver.save_screenshot(view["output"] + "page_01_error_02" + timestamp + '.png')
             continue
 
-        guichet_list[i].click()
-        nextButton.click()
-        time.sleep(s+random_number_gen(jit_s))
+        isloaded = False
+        trial = 0
+        while trial < NTRIAL :
+
+            trial+=1
+            time.sleep(s+random_number_gen(jit_s))
+            try:
+                headline = driver.find_element_by_id("inner_Booking")
+                guichet_list = driver.find_elements_by_class_name("radio")
+                nextButton = driver.find_element_by_name("nextButton")
+            except:
+                if not isloaded:
+                    headline = None
+                    guichet_list = None
+                    nextButton = None
+                    continue
+                break
+
+            if not guichet_list or not nextButton or not headline :
+                if not isloaded:
+                    continue
+                break
+
+            if not  page_2 in headline.text:
+                if not isloaded:
+                    continue
+                break             
+
+            try:
+                guichet_list[i].click()
+                nextButton.click()
+                isloaded=True
+            except:
+                driver.save_screenshot(view["output"] + "page_02_error_01" + timestamp + '.png')
+                
+        if trial >= NTRIAL:
+            driver.save_screenshot(view["output"] + "page_02_error_02" + timestamp + '.png')
+            continue
+            
+
+
 
         try:
             form = driver.find_elements_by_xpath("//form[@id='FormBookingCreate']")
         except:
             form = None
-            
+
         if not form:
+            driver.save_screenshot(view["output"] + "newwebpagewithoutform" + timestamp + '.png')
+            with codecs.open(view["output"] + "newwebpagewithoutform" + timestamp + '.html','w+',"utf-8") as fid:
+                fid.write(driver.page_source)
             continue
+
 
         if form[0].text == "Il n'existe plus de plage horaire libre pour votre demande de rendez-vous. Veuillez recommencer ultérieurement.":
             continue
   
-        
+        driver.save_screenshot(view["output"] + "newwebpagewithform" + timestamp + '.png')
 
-        try :
-            guichet_list = driver.find_elements_by_class_name("radio")
-            nextButton = driver.find_element_by_name("nextButton")
-        except:
-            guichet_list = None
-            nextButton = None
-
-        if not guichet_list or not nextButton:
-            driver.save_screenshot(view['output'] + timestamp + '.png')
-            continue
-
-        guichet_list[i].click()
-        nextButton.click()
-        time.sleep(s+random_number_gen(jit_s))
-
-        driver.save_screenshot(view['output'] + timestamp + '.png')
-
-        with codecs.open(view['output'] + timestamp + '.html','w+',"utf-8") as fid:
+        with codecs.open(view["output"] + "newwebpagewithform" + timestamp + '.html','w+',"utf-8") as fid:
             fid.write(driver.page_source)
 
         i = (i+1)%5
