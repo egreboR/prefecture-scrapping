bcd73904bae4a7e659c23ae746d32bbcf65626e2
Benoit Roberge <benoit.roberge05@gmail.com>
2021-09-04 21:03:06 +0200

    testing how to pass variable in bash fct v0.03

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
-
 touch '/home/data/$1.log'
 echo $1 2>> '/home/data/$1.log'
-python3 './web-scrapping/$1.py' 2>> '/home/data/$1.log'
+echo $CWD 2>> '/home/data/$1.log'
+echo 'TEST' 2>> '/home/data/$1.log'
+python3 './$1.py' 2>> '/$1.log'
 
